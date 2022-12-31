7b5207df5ebdfa26f60f17d0eca7c83a80f7e30d
Vinícius <vinigsouza.bnu@gmail.com>
2022-12-31 15:06:11 -0300

    feat: implementa base visual calculadora + limpezas de código

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -1,41 +1,27 @@
 <script setup>
 defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
 })
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      Main component
-    </h3>
+  <div class="place">
+    <div class="calc--body">
+    </div>
   </div>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
+.place {
+  background-color: rgb(228, 228, 228);
+  height: 100vh;
   text-align: center;
 }
 
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+.calc--body {
+  background-color: white;
+  margin: 0 auto;
+  border: 2px solid grey;
+  width: 30vw;
+  height: 75vh;
 }
 </style>
