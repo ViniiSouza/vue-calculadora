23756463f9f3ea633119d369814da9475e5f295c
Vinícius <vinigsouza.bnu@gmail.com>
2023-01-02 19:45:19 -0300

    feat: inclui importação de ícones utilizados

---
+++ src/icons/icons.js
@@ -0,0 +1,8 @@
+import { faXmark, faDivide, faMinus, faPlus, faEquals, faSquareRootVariable, faPercent, fa1, fa2, fa3, fa4, fa5, fa6, fa7, fa8, fa9, fa0, faA, faC, faClockRotateLeft, faArrowLeftLong } from "@fortawesome/free-solid-svg-icons";
+
+export default [
+                faXmark, faDivide, faMinus, faPlus, faEquals,
+                faSquareRootVariable, faPercent, fa1, fa2,
+                fa3, fa4, fa5, fa6, fa7, fa8, fa9, fa0, faA,
+                faC, faClockRotateLeft, faArrowLeftLong
+              ]
--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
-
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import './assets/main.css'
+import icons from './icons/icons'
+
+library.add(icons)
 
 const app = createApp(App)
 
 app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
