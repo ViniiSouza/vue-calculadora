4c4d9cb6f694f915859b1245fe96bdfa0beb275e
Vinícius <vinigsouza.bnu@gmail.com>
2023-01-03 22:27:06 -0300

    feat: cria array com as keys e informações

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -1,4 +1,8 @@
 <script setup>
+import { useCalculatorStore } from '../store/calcStore';
+import keys from './shared/keys'
+import CalcButton from './shared/components/CalcButton.vue';
+const { value } = useCalculatorStore()
 defineProps({
 })
 </script>
@@ -6,155 +10,25 @@ defineProps({
 <template>
   <div class="place">
     <div class="calc--body default-border">
-      <div class="calc--input default-border"></div>
+      <div class="calc--input default-border">
+        <p class="calc--input--value">{{ value }}</p>
+      </div>
       <div class="calc--board default-border">
-
-        <!-- Linha -->
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-c" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-a" />
-            <fa-icon icon="fa-c" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-arrow-left-long" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-clock-rotate-left" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-divide" />
-          </div>
-        </div>
-
-        <!-- Linha -->
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-7" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-8" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-9" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-square-root-variable" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-xmark" />
-          </div>
-        </div>
-
-        <!-- Linha -->
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-4" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-5" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-6" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-xmark" />²
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-minus" />
-          </div>
-        </div>
-
-        <!-- Linha -->
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-1" />
-
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-2" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-3" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-percent" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-plus" />
-          </div>
-        </div>
-
-        <!-- Linha -->
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <fa-icon icon="fa-0" />
-          </div>
-        </div>
-        <div class="calc--button">
-          <div class="calc--button--place">
-            <span class="m-1">
-              <fa-icon icon="fa-0" />
-            </span>
-            <fa-icon icon="fa-0" />
-          </div>
-        </div>
-        <div
-          style="font-weight: 900;"
-          class="calc--button"
-        >
-          <div class="calc--button--place">
-            ,
-          </div>
-        </div>
-        <div
-          class="calc--button"
-          style="width: 40%;"
-        >
-          <div class="calc--button--place">
-            <fa-icon icon="fa-equals" />
-          </div>
-        </div>
+        <calc-button
+          v-for="key in keys"
+          :key="key"
+          :icons="key.icons"
+          :width="key.width"
+          :key-detect="key.keyDetect"
+          :event="key.event"
+          :extra-text="key.extraText"
+        />
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style>
 .default-border {
   border: 2px solid rgb(240, 240, 240);
   border-radius: 5px;
@@ -180,6 +54,14 @@ defineProps({
   height: 8vh;
 }
 
+.calc--input--value {
+  color: white;
+  text-align: end;
+  font-size: 36px;
+  line-height: 10.5vh;
+  margin-right: 0.5vw;
+}
+
 .calc--board {
   display: inline-flex;
   flex-wrap: wrap;
@@ -191,33 +73,5 @@ defineProps({
 .calc-board--line {
   display: flex;
   height: 20%;
-}
-
-.calc--button {
-  color: rgb(240, 240, 240);
-  font-size: 28px;
-  display: flex;
-  width: 20%;
-  height: 20%;
-  border: 1px solid rgb(240, 240, 240);
-  justify-content: center;
-  align-items: center;
-}
-
-.calc--button--place {
-  cursor: pointer;
-  transition: 0.3s;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 100%;
-  width: 70px;
-  height: 70px;
-}
-
-.calc--button:hover .calc--button--place {
-  transition: 0.3s;
-  background-color: rgb(240, 240, 240);
-  color: rgb(29, 29, 29);
 }
 </style>
---
+++ src/components/shared/components/CalcButton.vue
@@ -0,0 +1,61 @@
+<script setup>
+defineProps({
+  icons: {
+    default: [],
+    type: Array
+  },
+  width: {
+    default: 1,
+    type: Number
+  },
+  keyDetect: {
+    default: 'none',
+    type: String
+  },
+  event: {
+    default: 'none',
+    type: String
+  },
+  extraText: {
+    default: '',
+    type: String
+  }
+})
+</script>
+<template>
+  <div class="calc--button" :style="`width: ${width * 20}%`">
+    <div class="calc--button--place">
+      <fa-icon v-for="icon in icons" :key="icon" :icon="icon" />
+      <span v-if="extraText">{{ extraText }}</span>
+    </div>
+  </div>
+</template>
+<style>
+.calc--button {
+  color: rgb(240, 240, 240);
+  font-size: 28px;
+  display: flex;
+  width: 20%;
+  height: 20%;
+  border: 1px solid rgb(240, 240, 240);
+  justify-content: center;
+  align-items: center;
+}
+
+.calc--button--place {
+  cursor: pointer;
+  transition: 0.3s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100%;
+  width: 70px;
+  height: 70px;
+}
+
+.calc--button:hover .calc--button--place {
+  transition: 0.3s;
+  background-color: rgb(240, 240, 240);
+  color: rgb(29, 29, 29);
+}
+</style>
